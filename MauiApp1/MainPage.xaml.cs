f5ab870cbd73232ed6e3b9843eb88a4aeec891d0
Etec Profº Horácio Augusto da Silveira <matheuspinter08@gmail.com>
2024-03-28 12:37:33 -0300

    feat: Adicionei a estrutura do projeto

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
