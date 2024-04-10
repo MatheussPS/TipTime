c6896f3a6e23b3b00dde7742e32e33d640903870
Matheus Pinter <matheuspinter08@gmail.com>
2024-04-10 18:40:38 -0300

    feat: Adicionei object senders, métodos de arredondamento e simplifiquei partes dos códigos

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,115 +1,98 @@
-﻿namespace MauiApp1
+﻿using System.Text.RegularExpressions;
+
+namespace MauiApp1
 {
     public partial class MainPage : ContentPage
     {
-        
+
 
         public MainPage()
         {
             InitializeComponent();
-            
+
+        }
+
+        private void Atualiazar_Valores(double sliderValue, float insertValue)
+        {
+            float gorj = (float)(insertValue * (sliderValue / 100));
+            double total = insertValue + gorj;
+
+            Percent_Vaue.Text = $"Porcentagem da Gorgeta: {sliderValue}%";
+            gorjLabel.Text = $"Gorgeta: R${Math.Round(gorj, 2)}";
+            Totals.Text = $"Total:  R$ {Math.Round(total, 2)}";
+
         }
 
         private void SliderGorgeta_ValueChanged(object sender, ValueChangedEventArgs e)
         {
             try
             {
-                Percent_Vaue.Text = $"Porcentagem da Gorgeta: {((float)SliderGorgeta.Value)}%";
+                double sliderValue = Math.Round(SliderGorgeta.Value);
                 float valor = Convert.ToSingle(quantiaEntry.Text);
-                float gorj = (float)(valor * (SliderGorgeta.Value / 100));
-                double tGorj = Math.Round(gorj, 2);
 
-                gorjLabel.Text = $"Gorgeta: R${tGorj}";
-                Totals.Text = $"Total:  R$ {Math.Round(tGorj + valor, 2)}";
+                Atualiazar_Valores(sliderValue, valor);
             }
             catch
             {
-                float i = 0;
-                Percent_Vaue.Text = $"Porcentagem da Gorgeta: {i}%";
-                gorjLabel.Text = $"Gorgeta: R${i}";
-                Totals.Text = $"Total:  R$ {i}";
+                Atualiazar_Valores(0, 0);
 
             }
         }
 
-
-        private void Set_fifteenPercentClicked(object sender, EventArgs e)
-        { 
-            
-            SliderGorgeta.Value = 15;
-            double valor = Convert.ToSingle(quantiaEntry.Text);
-            double gorj = (double)(valor * (SliderGorgeta.Value / 100));
-            double tGorj = Math.Round(gorj, 2);
-
-            gorjLabel.Text = $"Gorgeta: R${tGorj}";
-            Totals.Text = $"Total:  R$ {Math.Round(tGorj + valor, 2)}";
-        }
-
-        private void Set_thirtyPercentClicked(object sender, EventArgs e)
+        private void Set_PercentClicked(object sender, EventArgs e)
         {
-            SliderGorgeta.Value = 30;
-            double valor = Convert.ToSingle(quantiaEntry.Text);
-            double gorj = (double)(valor * (SliderGorgeta.Value / 100));
-            double tGorj = Math.Round(gorj, 2);
+            Button btn = (Button)sender;
 
-            gorjLabel.Text = $"Gorgeta: R${tGorj}";
-            Totals.Text = $"Total:  R$ {Math.Round(tGorj + valor, 2)}";
+            string btnValue = Regex.Replace(btn.Text, "%", "");
+            int btnValueConvert = Convert.ToInt32(btnValue);
+
+            SliderGorgeta.Value = btnValueConvert;
+
+            float valor = Convert.ToSingle(quantiaEntry.Text);
+
+            Atualiazar_Valores(btnValueConvert, valor);
 
         }
 
         private void Arrendondar_Btn_Clicked(object sender, EventArgs e)
         {
-            double valorAntes = Convert.ToSingle(quantiaEntry.Text);
-            double gorj = (double)(valorAntes * (SliderGorgeta.Value / 100));
-            double total = Math.Round(gorj + valorAntes, 0);
+            Button btn = (Button)sender;
+            double gorj;
+            double valor = Convert.ToSingle(quantiaEntry.Text);
 
-
-            if (total > gorj + valorAntes) {
-                total--;
-            }
-
-            Totals.Text = $"Total arredondado:  R$ {total}";
-
-
-        }
-
-        private void ArrendondarCima_Btn_Clicked(object sender, EventArgs e)
-        {
-            double valorAntes = Convert.ToSingle(quantiaEntry.Text);
-            double gorj = (double)(valorAntes * (SliderGorgeta.Value / 100));
-            double total = Math.Round(gorj + valorAntes, 0);
-
-            if (total < gorj + valorAntes)
+            if (btn.Text == "Arredondar para Baixo")
             {
-                total++;
+                gorj = Math.Floor((double)(valor * (SliderGorgeta.Value / 100)));
+            }
+            else
+            {
+                gorj = Math.Ceiling((double)(valor * (SliderGorgeta.Value / 100)));
             }
 
-            Totals.Text = $"Total arredondado:  R$ {total}";
+
+            double total = valor + gorj;
+
+            Totals.Text = $"Total arredondado:  R$ {total,2}";
 
 
         }
+
 
         private void QuantiaEntry_TextChanged(object sender, TextChangedEventArgs e)
         {
-            try 
-            { 
-                Percent_Vaue.Text = $"Porcentagem da Gorgeta: {((float)SliderGorgeta.Value)}%";
-                float valor = Convert.ToSingle(quantiaEntry.Text);
-                float gorj = (float)(valor * (SliderGorgeta.Value / 100));
-                double tGorj = Math.Round(gorj, 2);
-
-                gorjLabel.Text = $"Gorgeta: R${tGorj}";
-                Totals.Text = $"Total:  R$ {Math.Round(tGorj + valor, 2)}";
-            }
-            catch 
+            try
             {
-                float i = 0;
-                Percent_Vaue.Text = $"Porcentagem da Gorgeta: {i}%";
-                gorjLabel.Text = $"Gorgeta: R${i}";
-                Totals.Text = $"Total:  R$ {i}";
+                double sliderValue = Math.Round(SliderGorgeta.Value);
+                float valor = Convert.ToSingle(quantiaEntry.Text);
+
+                Atualiazar_Valores(sliderValue, valor);
+            }
+            catch
+            {
+                Atualiazar_Valores(0, 0);
 
             }
-            
+
         }
     }
 
